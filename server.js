e0e11f1622f97e489a1440a739bc2e6f2a40b01b
Mehdizade Orxan Kerem <Mehdi_ha01@itstep.edu.az>
2024-07-21 09:57:05 +0400

    newUp

--- server.js
+++ server.js
@@ -8,10 +8,31 @@ const productRoutes = require("./Routes/productRoutes");
 const orderRoutes = require("./Routes/orderRoutes");
 const basketRoutes = require("./Routes/basketRoutes");
 const PRODUCTDB_URL = process.env.CONNECTION_STRING;
-
+const cluster=require("cluster");
+const os = require("os");
 const app = express();
+const cpuNum=os.cpus().length;
+const port = 5000;
 app.use(express.json());
 app.use(cors());
+
+
+if(cluster.isMaster)
+{
+    for(let i=0; i<cpuNum;i++){
+        
+    }
+    cluster.on("exit",(worker)=>{
+        console. log(`worker with pid ${worker.process.pid} died`);
+        cluster.fork();
+    });
+
+}else{
+    app.listen(port, () => {
+        console.log(`Server running on ${process.pid} @${port}`);
+    });
+}
+
 
 mongoose
     .connect(PRODUCTDB_URL)
@@ -26,14 +47,7 @@ app.use("/orders", orderRoutes);
 app.use("/baskets", basketRoutes);
 
 
-port = 5000;
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Server running on port ${port}`);
+// });
 
-
-/*
-CONNECTION_STRING=mongodb+srv://mehdizadeorxan2000:WQaV9mNgEJxibxF0@todo.vbhj7ed.mongodb.net/?retryWrites=true&w=majority&appName=Todo
-ACCESS_TOKEN_SECRET=86990f24d9fcd37079aa218014a79481e78da327ef7f460f21ca9f61f122f7f0a35a69dbb5ad5333b1e6b9b577ebbf85d2c54237bc6a9abcd1d8544827d021ee
-REFRESH_TOKEN_SECRET=a718babe06d36ef4486a347a7212d3c1bb494b15e4731c730198b39a454cce1c98fd22ff9af2673d50cc03d88a2864e6645cf752fa94b5d427e36b290a13a102
-*/
